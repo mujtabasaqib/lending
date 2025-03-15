8eb5d2bc7eec3cd8e872945ae29508c35bb7747c
mujtabasaqib <mujtabasaqib99@gmail.com>
2025-03-15 17:07:39 +0500

    defined accounts for protocol

--- programs/lending/src/lib.rs
+++ programs/lending/src/lib.rs
@@ -1,16 +1,12 @@
 use anchor_lang::prelude::*;
 
+mod state;
+
 declare_id!("JCkkmdaUAmChf9eypcedCw7AtczLGuxmQbSqKpsZ6HvT");
 
 #[program]
 pub mod lending {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
+   
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
