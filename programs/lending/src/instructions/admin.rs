94573d2c7ad0cdab98fcb7340fd215df66993614
mujtabasaqib <mujtabasaqib99@gmail.com>
2025-03-26 16:13:08 +0500

    added interest calculations

--- programs/lending/src/instructions/admin.rs
+++ programs/lending/src/instructions/admin.rs
@@ -56,6 +56,7 @@ pub fn process_init_bank(ctx: Context<InitBank>, liquidation_threshold: u64, max
   bank.authority = ctx.accounts.signer.key();
   bank.liquidation_threshold = liquidation_threshold;
   bank.max_ltv = max_ltv;
+  bank.interest_rate = 0.05 as u64;
   Ok(())
 }
 
--- programs/lending/src/instructions/withdraw.rs
+++ programs/lending/src/instructions/withdraw.rs
@@ -1,3 +1,5 @@
+use std::f32::consts::E;
+
 use anchor_lang::prelude::*;
 use anchor_spl::{associated_token::AssociatedToken, token_interface::{self, Mint, TokenAccount, TokenInterface, TransferChecked}};
 
@@ -58,7 +60,22 @@ pub fn process_withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
     deposited_value = user.deposited_sol;
   }
 
+  /* 
   if amount > deposited_value {
+    return Err(ErrorCode::InsufficientFunds.into());
+  }
+  */
+
+  let time_diff = user.last_updated - Clock::get()?.unix_timestamp;
+
+  let bank = &mut ctx.accounts.bank;
+  bank.total_deposits = (bank.total_deposits as f64 * E.powf(bank.interest_rate as f32 * time_diff as f32) as f64)as u64;
+
+  let value_per_share = bank.total_deposits as f64 / bank.total_deposit_shares as f64;
+
+  let user_value = deposited_value as f64 / value_per_share;
+
+  if user_value < amount as f64 {
     return Err(ErrorCode::InsufficientFunds.into());
   }
 
